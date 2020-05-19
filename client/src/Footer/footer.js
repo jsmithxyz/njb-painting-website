69bd73ed8a152aac89867cb28d7008ecdfa2faf3
jsmithxyz <jordansmith3813@gmail.com>
2020-05-19 17:11:04 -0400

    added footer information

--- client/src/Footer/footer.js
+++ client/src/Footer/footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Container, NavbarBrand } from "react-bootstrap";
+import "./footer.css";
 
 class Footer extends Component {
   render() {
@@ -7,7 +8,20 @@ class Footer extends Component {
       <div className='footer'>
         <Navbar>
           <Container>
-            <NavbarBrand>Footer</NavbarBrand>
+            <NavbarBrand id='footer-text'>
+              <span>&copy;</span>2020 NJB Painting Inc. All Rights Reserved
+            </NavbarBrand>
+            <NavbarBrand id='footer-text'>
+              Website designed by{" "}
+              <a href='https://www.jsmithxyz.com/' target='_blank'>
+                jsmithxyz
+              </a>
+              . Icons by{" "}
+              <a href='https://icons8.com/' target='_blank'>
+                icons8
+              </a>
+              .
+            </NavbarBrand>
           </Container>
         </Navbar>
       </div>
